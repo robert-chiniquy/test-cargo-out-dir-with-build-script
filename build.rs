c2b6222835ae7bbc26cee9b02adafc887290fe59
Robert Chiniquy <robert.chiniquy@gmail.com>
2021-06-17 16:15:10 -0700

    A path attribute import in build.rs did not trigger it

--- build.rs
+++ build.rs
@@ -1,7 +1,12 @@
 use std::env;
 
+#[path = "src/lib.rs"]
+mod lib;
+
 fn main() {
     let out_dir = env::var_os("OUT_DIR").unwrap();
+
+    println!("cargo:warning={:?}", lib::IMPORT_THIS);
     println!(
         "cargo:warning=build.rs printing env:OUT_DIR -> {:?}",
         out_dir
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+pub const IMPORT_THIS: &str = "Testing if build.rs importing code from src/ triggers it";
+
 #[cfg(test)]
 mod test {
     const OUT_DIR: &str = env!("OUT_DIR");
